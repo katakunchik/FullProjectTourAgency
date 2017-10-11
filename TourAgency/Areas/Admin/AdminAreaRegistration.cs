6dcd3eb13d6ad6548fdcc01f47be987ae15dab97
Alex <katakunchik@gmail.com>
2017-10-11 23:44:02 +0300

    Add CountryController Index-Create-Edit

---
+++ TourAgency/Areas/Admin/AdminAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace TourAgency.Areas.Admin
+{
+    public class AdminAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Admin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Admin_default",
+                "Admin/{controller}/{action}/{id}",
+                new { controller = "Country",  action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ TourAgency/Areas/Admin/Controllers/CountryController.cs
@@ -0,0 +1,69 @@
+﻿using BLL.DTO;
+using BLL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TourAgency.Areas.Admin.Controllers
+{
+    public class CountryController : Controller
+    {
+        private readonly ILocationService _locationService;
+        public CountryController(ILocationService locationService)
+        {
+            _locationService = locationService;
+        }
+        
+        // GET: Admin/Country
+        public ActionResult Index()
+        {
+            var model = _locationService.Countries();
+            return View(model);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public ActionResult Create(CountryCreateViewModel countryModel)
+        {
+            if(ModelState.IsValid)
+            {
+                var operationStatus = _locationService.CountryCreate(countryModel);
+                if(operationStatus == CountryStatusViewModel.Success)
+                    return RedirectToAction("Index");
+                else if(operationStatus == CountryStatusViewModel.Dublication)
+                    ModelState.AddModelError("", "Країна з даним іменем вже існує");
+            }
+            return View(countryModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var countryModel = _locationService.GetCountryEditById(id);
+            return View(countryModel);
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public ActionResult Edit(CountryEditViewModel countryModel)
+        {
+            if(ModelState.IsValid)
+            {
+                var operationStatus = _locationService.CountryEdit(countryModel);
+                if (operationStatus == CountryStatusViewModel.Success)
+                    return RedirectToAction("Index");
+                else if (operationStatus == CountryStatusViewModel.Dublication)
+                    ModelState.AddModelError("", "Країна з даним іменем вже існує");
+                else if (operationStatus == CountryStatusViewModel.Error)
+                    ModelState.AddModelError("", "Помилка редагування");
+            }
+            return View(countryModel);
+        }
+    }
+}
